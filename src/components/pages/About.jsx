c6eb1b30d4bc1f49067504c48512b733c821bee2
Белова Анна (Halyk Finservice) <annabel@halykbank.nb>
2020-12-25 17:56:01 +0600

    first build

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList aoo v1.0.0 Ut is part of a 
+        React crash course</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
